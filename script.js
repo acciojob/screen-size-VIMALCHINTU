8d62d9a86923c186d8e105c4a000038f92a7fdff
VIMALCHINTU <158822830+VIMALCHINTU@users.noreply.github.com>
2025-09-13 17:57:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ let ptag=document.getElementById("sizeInfo")
     window.addEventListener("resize",()=>{
       const height=window.innerHeight
       const width=window.innerWidth 
-      ptag.textContent=`Width: ${width} and Height: ${width}`
+      ptag.textContent=`Width: ${width} and Height: ${height}`
     }
   )
